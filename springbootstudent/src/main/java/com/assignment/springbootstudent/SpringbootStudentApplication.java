f76ba708dbe841fcfb518d36ac689f010e71311c
Shreya Joshi <116377209+Shreya-0699@users.noreply.github.com>
2023-03-30 16:08:13 +0530

    Add files via upload

---
+++ springbootstudent/src/main/java/com/assignment/springbootstudent/SpringbootStudentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.springbootstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudentApplication.class, args);
+	}
+
+}
---
+++ springbootstudent/src/test/java/com/assignment/springbootstudent/SpringbootStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.springbootstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
